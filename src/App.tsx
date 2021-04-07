1049ceaa683444d5e7172a5fcb378c076d5a5072
wanglihua <1106990976@qq.com>
2021-04-07 10:02:42 +0800

    [Add]: rxjs to retry request

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
+import { of, Observable, defer, from, forkJoin } from "rxjs";
+import {
+  map,
+  switchMap,
+  tap,
+  retry,
+  catchError,
+  retryWhen,
+  mergeMap,
+} from "rxjs/operators";
 const m3u8Parser = require("m3u8-parser");
 const muxjs = require("mux.js");
-
 const url =
   "https://static.uskid.com/playback/20200711/j3gdn354k/2_LTXKZ36xE7rhCZG9.m3u8";
 const host = url.match(/(.*)\/.*.m3u8$/)?.[1] ?? "";
@@ -78,12 +87,86 @@ function App() {
     };
     mediaSource.addEventListener("sourceopen", appendFirstSegment);
   };
+  // useEffect(() => {
+  //   doTask()
+  //     .then()
+  //     .catch((error) => {
+  //       console.log(`error`, error);
+  //     });
+  // }, []);
   useEffect(() => {
-    doTask()
-      .then()
-      .catch((error) => {
-        console.log(`error`, error);
+    const testUrl =
+      // "https://static.uskid.com/playback/20200523/qn5w3mn75/2_6gr4jM07dBixDv4D.m3u8";
+      // "https://uskid.oss-cn-beijing.aliyuncs.com/playback/20200523/qn5w3mn75/2_6gr4jM07dBixDv4D.m3u8";
+      "https://uskid.oss-accelerate.aliyuncs.com/playback/20200523/qn5w3mn75/2_6gr4jM07dBixDv4D.m3u8";
+    // "https://static.uskid.com/playback/20200711/j3gdn354k/2_LTXKZ36xE7rhCZG9.m3u8";
+    const host = testUrl.match(/(.*)\/.*.m3u8$/)?.[1] ?? "";
+
+    const getM3U8 = (url: string) =>
+      defer(() => {
+        console.log(`调用中------`);
+        return axios.get(url);
+      }).pipe(
+        map((item: any) => {
+          return item.data;
+        }),
+        retryWhen((error: any) => {
+          console.log(error);
+          return error;
+        }),
+        catchError((err) => {
+          return of({ error: true, message: err.message });
+        })
+      );
+    const urlMap = new Map();
+    const getTs$ = (url: string) =>
+      defer(() => {
+        urlMap.set(url, (urlMap.get(url) ?? 0) + 1);
+        if (urlMap.get(url) > 1) {
+          console.log(`重试中`, url, urlMap.get(url));
+        }
+        return axios.get(url, {
+          responseType: "arraybuffer",
+        });
+      }).pipe(
+        map((item: any) => {
+          return item.data;
+        }),
+        retry(10)
+        // retryWhen((error: any) => {
+        //   console.log(error);
+        //   return error;
+        // })
+      );
+    const format = (m3u8File: any) => {
+      const parser = new m3u8Parser.Parser();
+      parser.push(m3u8File);
+      parser.end();
+      const urls = parser.manifest.segments
+        .map(({ uri }) => uri)
+        .map((uri) => `${host}/${uri}`);
+      return urls;
+    };
+    const data$ = getM3U8(testUrl);
+    const task = async (m3u8File) => {
+      const tss = format(m3u8File);
+      const segments$ = of(tss).pipe(
+        mergeMap((items) => forkJoin(...items.map(getTs$))),
+        catchError((err) => {
+          console.log(`error`, err);
+          return of({ error: true, message: err.message });
+        })
+      );
+      segments$.subscribe({
+        next: console.log,
+        error: console.log,
+        complete: () => console.log("segments-done"),
       });
+    };
+    data$.subscribe({
+      next: task,
+      complete: () => console.log("data-done"),
+    });
   }, []);
   return (
     <div className="App">
